54e3e2d30781f207bd0b568440810b3ba99eed3b
Kiel-P <kielhjs6@gmail.com>
2025-11-30 18:24:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from app.menus.store.segments import show_store_segments_menu
 from app.menus.store.search import show_family_list_menu, show_store_packages_menu
 from app.menus.store.redemables import show_redeemables_menu
 from app.client.registration import dukcapil
-#from app.client.sharing import bounty_allotment_menu
+from app.client.sharing import bounty_allotment_menu
 
 # NEW IMPORTS FOR UI
 from rich.table import Table
@@ -240,7 +240,7 @@ def main():
                 pause()
 
             elif choice.lower() == "tf":
-                show_balance_allotment_menu()
+                show_bounty_allotment_menu()
             elif choice.lower() == "v":
                 msisdn = cyber_input("Enter the msisdn to validate (628xxxx)")
                 with loading_animation("Validating..."):
